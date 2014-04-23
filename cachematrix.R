a40bd0cdf9ba8d29e68759da688fa1cee84ebe6f
deenoo82 <alexchang_82@yahoo.com>
2014-04-22 21:59:43 -0700

    Remove some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
-# This purpose of the functoin is the to calculate the inverse and cache the
-# result and return from result from the calculation or cache if already been
-# calculated before.  Assume that the matrix is always invertible
+## This purpose of the functoin is the to calculate the inverse and cache the
+## result and return from result from the calculation or cache if already been
+## calculated before.  Assume that the matrix is always invertible
 
 ## Write a short comment describing this function
-# a function that create a special "matrix", which is a function to
-# set the value of the matrix
-# get the value of the matrix
-# set the value of the inverse matrix
-# get the value of the inverse matrix
+## a function that create a special "matrix", which is a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse matrix
+## get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     ## special "matrix"
@@ -27,10 +25,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-# This function will return the inverse of matrix from cache (of the same matrix)
-# when available otherwise calcuate and store the cache of matrix for future
-# result
+
+## This function will return the inverse of matrix from cache (of the same matrix)
+## when available otherwise calcuate and store the cache of matrix for future
+## result
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
